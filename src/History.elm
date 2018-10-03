f5991ee0cc3d5ec17ffd79cc46cf4a505fcc82f9
Jindrich K. Smitka <jsmitka@smita.info>
2018-10-03 06:38:53 +0200

    Refactored history to store more past data

--- src/History.elm
+++ src/History.elm
@@ -1,10 +1,30 @@
-module History exposing (create)
+module History exposing (create, current, forward, rewind)
 
 
 type History a
-    = History a (Maybe a)
+    = History a (List a)
 
 
 create : a -> History a
 create value =
-    History value Nothing
+    History value []
+
+
+forward : a -> History a -> History a
+forward value (History old past) =
+    History value (old :: past)
+
+
+rewind : History a -> History a
+rewind ((History _ past) as history) =
+    case past of
+        old :: remains ->
+            History old remains
+
+        [] ->
+            history
+
+
+current : History a -> a
+current (History cur _) =
+    cur
